1f9dc6646cd47d456069cd82f971891252234cbe
Matthew Guo <mjguo@ucsc.edu>
2024-01-27 00:09:12 -0800

    Update sketch.js
    More flags for green and orange random walks, Math.round() for random values

--- experiment3/Experiment_3_Project/sketch.js
+++ experiment3/Experiment_3_Project/sketch.js
@@ -1,13 +1,16 @@
 let points1 = [];
 let points2 = [];
 let points3 = [];
-let intersected;
+let points4 = [];
+let isGreenWalk;
+let isOrangeWalk;
 
 function setup() {
-  createCanvas(400, 400);
-  points1.push(createVector(200, 200));
-  points2.push(createVector(300, 300));
-  intersected = false;
+  createCanvas(800, 500);
+  points1.push(createVector(Math.round(random(300, 500)), Math.round(random(200, 400))));
+  points2.push(createVector(Math.round(random(300, 500)), Math.round(random(200, 400))));
+  isGreenWalk = false;
+  isOrangeWalk = false;
   strokeWeight(5);
 }
 
@@ -17,22 +20,29 @@ function draw() {
   doRandomWalk(points1);
   stroke(0, 0, 255);
   doRandomWalk(points2);
-  if(intersected) {
+  if(isGreenWalk) {
     stroke(0, 255, 0);
     doRandomWalk(points3);
   }
   else {
-    checkForIntersection(points1, points2, points3);
+    isGreenWalk = checkForIntersection(points1, points2, points3);
+  }
+  if(isGreenWalk && isOrangeWalk) {
+    stroke(255, 165, 0);
+    doRandomWalk(points4);
+  }
+  else {
+    isOrangeWalk = checkForIntersection(points2, points3, points4)
   }
 }
 
 function doRandomWalk(point_arr) {
   let x_val = point_arr[point_arr.length - 1].x;
   let y_val = point_arr[point_arr.length - 1].y;
-  x_val += floor(random(-5, 5));
-  y_val += floor(random(-5, 5));
-  x_val = constrain(x_val, 0, width - 1);
-  y_val = constrain(y_val, 0, height - 1);
+  x_val += Math.round(random(-10, 10));
+  y_val += Math.round(random(-10, 10));
+  x_val = constrain(x_val, 0, width);
+  y_val = constrain(y_val, 0, height);
   point_arr.push(createVector(x_val, y_val)); 
   for (let i = 0; i < point_arr.length; i++) {
     point(point_arr[i].x, point_arr[i].y);
@@ -44,7 +54,8 @@ function checkForIntersection(rand1, rand2, rand3) {
   for (let i = 0; i < rand2.length; i++) {
     if (lastPoint.x == rand2[i].x && lastPoint.y == rand2[i].y) {
       rand3.push(createVector(lastPoint.x, lastPoint.y));
-      intersected = true;
+      return true;
     }
   }
+  return false;
 }
