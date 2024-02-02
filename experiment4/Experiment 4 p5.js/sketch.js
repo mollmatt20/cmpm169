6dac4779efd0b5499e9bc5fab6c18a44c58fc1c2
Matthew Guo <mjguo@ucsc.edu>
2024-02-02 11:40:46 -0800

    Updated experiment with main image and sound

--- experiment4/Experiment 4 p5.js/sketch.js
+++ experiment4/Experiment 4 p5.js/sketch.js
@@ -1,33 +1,39 @@
-'use strict';
-
-var img;
+let img;
+let sound;
+let x1, y1, x2, y2, w, h;
 let isDrawing;
 
 function preload() {
-  img = loadImage('data/pic.png');
+  img = loadImage('assets/persistenceofmemory.jpg');
+  sound = loadSound('assets/bad_dream.mp3');
 }
 
 function setup() {
-  createCanvas(1024, 780);
-  image(img, 0, 100);
+  createCanvas(620, 430);
+  image(img, 0, 0);
   isDrawing = false;
+  sound.setVolume(0.3);
+  sound.setLoop(true);
 }
 
 function draw() {
   if(isDrawing) {
-    var x1 = mouseX;
-    var y1 = mouseY;
-  
-    var x2 = round(x1);
-    var y2 = round(y1 + random(1, 5));
-
-    var w = 100;
-    var h = 100;
-
+    x1 = mouseX;
+    y1 = mouseY;
+    x2 = round(x1);
+    y2 = round(y1 + random(1, 5));
+    w = random(50, 80);
+    h = random(20, 50);
     set(x2, y2, get(x1, y1, w, h));
   }
 }
 
 function mousePressed() {
   isDrawing = !isDrawing;
+  if(isDrawing) {
+    sound.play();
+  }
+  else {
+    sound.pause();
+  }
 }
